d3449fd63825e0c914f1bd4cca2c2d121c7a3860
Christopher Dombroski <dombroski.christopher@gmail.com>
2014-02-12 21:46:37 -0800

    Progress towards AABB collisions

--- src/com/Entity.java
+++ src/com/Entity.java
@@ -5,22 +5,19 @@ import physics.Physics;
 public class Entity {
 	float[] vertices;
 	float[] normals;
-	float[] textures; //float??
-	
+	float[] textures; // float??
+
 	String name;
-	
+
 	short type;
 	
-//	final short SHAPE = 0;
-//	final short CONSTRAINT_STRING = 1;
-//	final short CONSTRAINT_ELASTIC = 2;
-//	final short CONSTRAINT_ROD = 3;
-//	final short CONSTRAINT_SPRING = 4;
-	
-	public static void stuff() {
-	
+	Entity(String name, short type, float[] vertices, float[] normals, float[] textures) {
+		this.name = name;
+		this.vertices = vertices;
+		this.normals = normals;
+		this.textures = textures;
 	}
-	
+
 	Physics physObject;
-	
+
 }
